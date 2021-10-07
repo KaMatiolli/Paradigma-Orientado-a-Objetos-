17d522e91c174c4db0f06caa3bfa1b104c203ea9
Karina Matiolli <70562104+KaMatiolli@users.noreply.github.com>
2021-10-07 15:47:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,43 @@
+[
+  class Main {
+  public class Produto {
+
+    private String nome;
+    private String marca;
+    private double peso;
+    private double preço;
+
+    public Produto(String nome, String marca, double peso, double preço){
+      this.nome = nome;
+      this.marca = marca;
+      this.peso = peso;
+      this.preço = preço;
+    }
+
+    public String getNome() {
+      return nome;
+    }
+    public void setNome() {
+      this.nome = nome;
+    }
+
+    public String getMarca() {
+      return marca;
+    }
+    public void setNome() {
+      this.nome = nome;
+    }
+
+    public int getPeso{
+     return peso;
+    }
+    public void setPeso(int peso){
+    this.peso = peso;
+    }
+
+    public int getPreço{
+     return preço;
+    }
+    public void setPreço(int preço){
+    this.preço = preço;
+}
